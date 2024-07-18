21597edb3ddf88136acf6aa5300a57b0258a4a56
katnamimo <44755024+katnamimo@users.noreply.github.com>
2024-07-17 19:55:25 -0700

    able to add image to create listing

--- lib/Pages/MakePost/makepost.dart
+++ lib/Pages/MakePost/makepost.dart
@@ -1,227 +1,228 @@
-import 'package:eco_connect/all_listings_page.dart';
-import 'package:eco_connect/globalstate.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'dart:io';
+import 'dart:typed_data';
 
-import 'package:eco_connect/globalstate.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MyMakePostPage extends StatefulWidget {
-  const MyMakePostPage({super.key});
+  const MyMakePostPage({Key? key}) : super(key: key);
 
   @override
   State<MyMakePostPage> createState() => _MyMakePostPageState();
 }
-final List<String> serviceTypes = ['Housecleaning', 'Babysitting', 'News', 'Events', 'Lawn Care', 'Garden', 'Compost', 'Carpool', 'Other']; // List of service options
+
+final List<String> serviceTypes = [
+  'Housecleaning',
+  'Babysitting',
+  'News',
+  'Events',
+  'Lawn Care',
+  'Garden',
+  'Compost',
+  'Carpool',
+  'Other'
+]; // List of service options
 String selectedService = serviceTypes[0]; // Initial selection
 
 class _MyMakePostPageState extends State<MyMakePostPage> {
-  final _titleController = TextEditingController();
-  final _bodyController = TextEditingController();
-  String name = "";
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _bodyController = TextEditingController();
+  File? _image;
+  Uint8List? _webImage;
+  String? _imagePath;
 
-  void initState() {
-    super.initState();
-    fetchUserData();
-  }
-
-  void fetchUserData() async {
-    final String phoneNumber = Provider.of<UserState>(context, listen: false).phone;
-
-    final database = FirebaseDatabase.instance.ref();
-    final snapshot = await database.child('users').orderByChild('phone').equalTo(phoneNumber).get();
-
-    if (snapshot.exists) {
-      final userData = snapshot.value as Map<dynamic, dynamic>;
-      final userKey = userData.keys.first;
-      final user = userData[userKey];
-
-      setState(() {
-        name = user['name'] ?? 'No name';
-      });
-    } else {
-      setState(() {
-        name = 'No user found';
-      });
-    }
-  }
-
-  Future<void> writePost({required String author, required String title, required String body, required String service}) async {
-    // gets the UTC time and date at the moment of post
-    final now = DateTime.now();
-    //root database reference
-    //final String phoneNumber = Provider.of<UserState>(context, listen: false).phone;
-    final database = FirebaseDatabase.instance.ref();
-    final Map<String, dynamic> post = {
-      'author': author,
-      'personal id': Provider.of<UserState>(context, listen: false).phone,
-      'title' : title,
-      'body' : body,
-      'time' : now.toString(),
-      'serviceType' : service,
-    };
-
+  // Method to handle image selection
+  Future<void> _pickImage() async {
     try {
-      // Push data to the 'users' node with a unique key
-      await database.child('posts').push().set(post);
-      // Show success message or perform other actions (optional)
-      print('Phone number written successfully!');
-    } on FirebaseException catch (e) {
-      // Handle potential errors during data writing
-      print('Error writing data: $e');
+      final pickedFile = await ImagePicker().getImage(source: ImageSource.gallery);
+      if (pickedFile != null) {
+        if (kIsWeb) {
+          final bytes = await pickedFile.readAsBytes();
+          setState(() {
+            _webImage = bytes;
+            _image = null; // Reset _image if previously set
+            _imagePath = null; // Reset _imagePath if previously set
+          });
+        } else {
+          setState(() {
+            _image = File(pickedFile.path);
+            _webImage = null; // Reset _webImage if previously set
+          });
+        }
+      } else {
+        print('No image selected.');
+      }
+    } catch (e) {
+      print('Error picking image: $e');
     }
-  }
-
-
-  @override
-  void dispose() {
-    //releases memory
-    _titleController.dispose();
-    _bodyController.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
+      appBar: AppBar(
+        title: Text('Create a Listing'),
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
         color: const Color(0xFF121212),
-        // Set the background color to dark grey.
         alignment: Alignment.center,
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 60.0),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
         child: SingleChildScrollView(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                    child: Text(
-                      "Create a Listing",
-                      style: TextStyle(
-                        fontSize: 24, // Slightly larger text
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  _pickImage();
+                },
+                child: Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(40),
                   ),
-                const SizedBox(height: 50),
-          Container(
-            width: 640,
-            child: TextField(
-              controller: _titleController,
-              decoration: const InputDecoration(
-                hintText: 'Title',
-                hintStyle: TextStyle(color: Color(0xFFB3B3B3)),
-                enabledBorder: const OutlineInputBorder(
-                  // width: 0.0 produces a thin "hairline" border
-                  borderSide: const BorderSide(color: Colors.grey, width: 0.0),
                 ),
-                border: OutlineInputBorder(),
-                fillColor: Color(0xFF212121),
-                filled: true,
               ),
-              style: const TextStyle(color: Color(0xFFB3B3B3)),
-            ),
+              const SizedBox(height: 10),
+              const Text(
+                'Select an image for your post',
+                style: TextStyle(fontSize: 14, color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              _image != null || _webImage != null
+                  ? Image.memory(
+                _webImage!,
+                width: double.infinity,
+                height: 200,
+                fit: BoxFit.cover,
+              )
+                  : Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.grey[800],
+                child: Center(
+                  child: Text(
+                    'No Image Selected',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: double.infinity,
+                child: TextField(
+                  controller: _titleController,
+                  decoration: const InputDecoration(
+                    hintText: 'Title',
+                    hintStyle: TextStyle(color: Color(0xFFB3B3B3)),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 0.0),
+                    ),
+                    border: OutlineInputBorder(),
+                    fillColor: Color(0xFF212121),
+                    filled: true,
+                  ),
+                  style: const TextStyle(color: Color(0xFFB3B3B3)),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: double.infinity,
+                child: TextField(
+                  controller: _bodyController,
+                  maxLines: 8,
+                  decoration: InputDecoration(
+                    alignLabelWithHint: true,
+                    hintText: 'Share with your community',
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    border: OutlineInputBorder(),
+                    fillColor: Color(0xFF212121),
+                    filled: true,
+                  ),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                height: 50,
+                width: double.infinity,
+                color: Color(0xFF212121),
+                child: DropdownButtonFormField<String>(
+                  value: selectedService,
+                  decoration: const InputDecoration(
+                    labelStyle: TextStyle(color: Color(0xFFB3B3B3)),
+                    hintText: '',
+                    fillColor: Color(0xFF212121),
+                    filled: true,
+                  ),
+                  style: const TextStyle(
+                    color: Color(0xFFB3B3B3),
+                  ),
+                  dropdownColor: Color(0xFF212121),
+                  items: serviceTypes
+                      .map((serviceType) => DropdownMenuItem<String>(
+                    value: serviceType,
+                    child: Text(serviceType),
+                  ))
+                      .toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedService = newValue!;
+                    });
+                  },
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_titleController.text.isNotEmpty &&
+                        _bodyController.text.isNotEmpty) {
+                      print('Author: ${_titleController.text}');
+                      print('Body: ${_bodyController.text}');
+                      print('Service: $selectedService');
+                      if (_imagePath != null) {
+                        print('Image path: $_imagePath');
+                      }
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Fill in all fields.'),
+                          backgroundColor: Colors.red,
+                        ),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF1DB954),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 12.0, horizontal: 20.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  child: const Text(
+                    'Post',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-                  const SizedBox(height: 30),
-                  Container(
-                    width: 640,
-                    height: 200,
-                    child: TextField(
-                      controller: _bodyController,
-                      maxLines: 8,
-                      decoration: InputDecoration(
-                        alignLabelWithHint: true,
-                        hintText: 'Share with your community',
-                        hintStyle: TextStyle(color: Colors.grey[400]),
-                        border: OutlineInputBorder(),
-                        fillColor: Color(0xFF212121),
-                        filled: true,
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    height: 50,
-                    width: 640,
-                    color: Color(0xFF212121),
-
-                    child: DropdownButtonFormField<String>(
-                      value: selectedService, // Set initial value
-                      decoration: const InputDecoration(
-                        labelStyle: TextStyle(color: Color(0xFFB3B3B3)),
-                        hintText: '',
-                        fillColor: Color(0xFF212121),
-                        filled: true,
-                      ),
-                      style: const TextStyle(
-                        color: Color(0xFFB3B3B3),
-                      ),
-
-                      dropdownColor: Color(0xFF212121),
-                      focusColor: Color(0xFF212121),
-
-                      items: serviceTypes.map((serviceType) => DropdownMenuItem<String>(
-                        value: serviceType,
-                        child: Text(serviceType),
-                      )).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          selectedService = newValue!; // Update selectedService
-                        });
-                      },
-                    ),
-                  ),
-
-                  SizedBox(height: 30),
-                  Container(
-                    width: 640,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_titleController.text != '' && _bodyController.text != '') {
-                          writePost(
-                              author: name,
-                              title: _titleController.text,
-                              body: _bodyController.text,
-                              service: selectedService);
-                          Navigator.pop(context);
-                        }
-                        else {
-                          // empty fields
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('Fill in all fields.'),
-                              backgroundColor: Colors.red,
-                            ),
-                          );
-                        }
-                        },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF1DB954),
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                      ),
-                      child: const Text(
-                        'Post',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-            ),
-            )
-    )
-
+        ),
+      ),
     );
-
   }
+}
 
+void main() {
+  runApp(MaterialApp(home: MyMakePostPage()));
 }
