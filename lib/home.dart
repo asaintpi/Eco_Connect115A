9c70cf1806ef31ac28bf7285720d852ec65d807c
ebarnes2 <ellbarne@ucsc.edu>
2024-07-22 19:04:09 -0700

    google commented out

--- lib/home.dart
+++ lib/home.dart
@@ -271,7 +271,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-            const SizedBox(height: 20),
+            /*const SizedBox(height: 20),
             SizedBox(
               width: 640,
               height: 50,
@@ -296,6 +296,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
+            */
+
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _showEmailPasswordSignInDialog,
